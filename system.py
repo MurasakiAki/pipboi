66be40f75a60984a7451b23ff7e83579d47f2387
MurasakiAki <barnabas.bernasek@gmail.com>
2023-09-25 19:41:42 +0200

    code cleanup

--- system.py
+++ system.py
@@ -11,43 +11,7 @@ import socket
 from PIL import Image
 import calendar
 
-# normal 0-2
-# love 3-5
-# surprise 6-8
-# disappointment 9-11
-# sad 12-14
-# angry 15-17
-emotions = ["'V'", ">v<", "◠‿◠", "¬‿¬✿", "◕ω◕✿", "｡♥‿♥｡", "☉_☉", "'o'", "⊙△⊙", "<_<", "¬_¬", "⌣_⌣”", "QvQ", "ಥ﹏ಥ", "╥﹏╥", "╬ Ò﹏Ó", "=_=", "⋋▂⋌"]
 farewells = ["Bye", "Bye bye!", "Have a good day!", "See you soon!", "See ya!"]
-
-# Emotions
-def gen_random():
-     
-    return random.choice(emotions)
-
-def gen_normal():
-
-    return emotions[random.randint(0, 2)]
-
-def gen_love():
-
-    return emotions[random.randint(3, 5)]
-
-def gen_surprise():
-    
-    return emotions[random.randint(6, 8)]
-
-def gen_disappointment():
-
-    return emotions[random.randint(9, 11)]
-
-def gen_sad():
-
-    return emotions[random.randint(12, 14)]
-
-def gen_angry():
-    
-    return emotions[random.randint(15, 17)]
 
 #Greetings/Farewells
 def rnd_farewell():
